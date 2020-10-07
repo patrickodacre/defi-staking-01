2deb000107c03d1fe56a618c527622c0143185b0
PWho <patrick@patrickwho.me>
2020-10-07 12:22:23 -0400

    Add Connect to ethereum in Navbar
    Here I deviate from Gregory's template
    a bit; MetaMask docs urge developers to only
    establish a connection with a wallet when a
    user specifically requests it:
    
    From the MetaMask docs:
    "You should only initiate a connection request
    in response to direct user action, such as
    clicking a button. You should never initiate a
    connection request on page load."
    
    To keep in-step with this direction, I moved
    the connection code to the Navbar and initiate
    the connection only when the user clicks a btn.
    
    Thoughts:
    - Should the connection code be with the Navbar?
    At this stage, the easiest place to put this code
    is in the Navbar; that's where the button is,
    that's where the account address is being displayed.
    I could get more fancy with this to further isolate
    the connection code, but I don't gain much by doing
    that so soon.

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,13 @@ class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {
-      account: '0x0'
-    }
   }
 
   render() {
     return (
       <div>
-        <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
+        <Navbar />
+        <div className="container-fluid">
           <div className="row">
             <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
               <div className="content mr-auto ml-auto">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react'
 import farmer from '../farmer.png'
+import Web3 from 'web3'
 
 class Navbar extends Component {
 
+    async loadData() {
+        const [account] = await window.web3.eth.getAccounts()
+
+        this.setState({account})
+    }
+
+    async loadWeb3() {
+        // ethereum object is set by
+        // https://eips.ethereum.org/EIPS/eip-1193
+        // compliant providers like MetaMask
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        }
+        else if (Web3 && Web3.currentProvider) {
+            window.web3 = new Web3(Web3.currentProvider)
+        } else {
+            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        }
+    }
+
+    constructor(props) {
+        super(props)
+
+        this.loadWeb3 = this.loadWeb3.bind(this)
+        this.loadData = this.loadData.bind(this)
+        this.loadEth = this.loadEth.bind(this)
+
+        this.state = {
+            account: null,
+        }
+    }
+
+    async loadEth() {
+        await this.loadWeb3()
+        await this.loadData()
+    }
+
   render() {
+
+      let accountButton
+
+      if (this.state.account) {
+          accountButton = <small className="text-secondary">
+              <small id="account">{this.state.account}</small>
+              </small>
+      } else {
+          accountButton = <button class="btn btn-primary" onClick={this.loadEth}>Enable Ethereum</button>
+      }
+
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar navbar-dark bg-dark flex-md-nowrap mb-4 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="http://www.dappuniversity.com/bootcamp"
@@ -18,9 +68,7 @@ class Navbar extends Component {
 
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-            <small className="text-secondary">
-              <small id="account">{this.props.account}</small>
-            </small>
+            {accountButton}
           </li>
         </ul>
       </nav>
